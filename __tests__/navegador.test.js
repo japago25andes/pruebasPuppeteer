04ad97c4678321dea25d8ff30e82db69af127ebc
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2023-12-13 23:20:14 -0500

    Configuracion de opciones de lanzamiento o apertutra del navegador y su ventana

--- __tests__/navegador.test.js
+++ __tests__/navegador.test.js
@@ -3,11 +3,19 @@ const puppeteer = require("puppeteer")
 describe("Mi primer test en puppeteer", () => {
 	it("Debe abrir y cerrar el navegador", async () => {
 		const browser = await puppeteer.launch({
-			headless: false,
+			headless: false,//que se abra o no el navedagor en mi pantalla (ginete sin cabeza). headless: false abre el navegador
+            slowMo: 1000, //camara lenta para que se pueda ver mejor la interacion con el navegador
+            devtools: false, //abre por decir algo el inspeccionar del navegador
+            //defaultViewport:{//cambia el tamaño de la pantalla interna
+            //    width: 2100,
+            //    height: 1080
+            //},
+            //args: ['--window-size=1920,1080']//modifica e tamaño de la pantallas o de la ventana para ser exactos
+            // defaultViewport: null//modifica el ancho interno de la ventana para ajustarse al que se abre
 		})
 		const page = await browser.newPage()
 		await page.goto("https://www.google.com")
 		await page.waitForTimeout(5000)
 		await browser.close()
-	}, 10000)
+	}, 30000)
 })
