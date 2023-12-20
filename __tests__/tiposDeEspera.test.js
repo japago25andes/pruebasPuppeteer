0af922f29165e0cdf03d2f9970b02c72ac9e6f45
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2023-12-20 06:14:04 -0500

    Esperando por funciones

--- __tests__/tiposDeEspera.test.js
+++ __tests__/tiposDeEspera.test.js
@@ -5,6 +5,7 @@ describe("Tipos de espera", () => {
 		const browser = await puppeteer.launch({
 			headless: false, //que se abra o no el navedagor en mi pantalla (ginete sin cabeza). headless: false abre el navegador
 			defaultViewport: null, //modifica el ancho interno de la ventana para ajustarse al que se abre
+
 		})
 		const page = await browser.newPage()
         await page.goto("https://www.yahoo.com/", {waitUntil: 'networkidle0'})
@@ -27,6 +28,19 @@ describe("Tipos de espera", () => {
         await button.click()
 		await page.waitForTimeout(500)
         
+        
+        //Espera por funcion
+        //Se toma el selector y se revisa que en el selector aparezca el texto que se envia como argumento
+		await page.waitForFunction(() => document.querySelector('#example-modal-sizes-title-sm').innerText === 'Small Modal')
+        
+        //Ejemplo para observar el viewport
+        const observaResize = page.waitForFunction('window.innerWidth < 100')
+        await page.setViewport({width:50, height:50})
+        await observaResize  
+        
+        //Se cierra la ventana emergente y seguidose valida con negacion que ya no este presente
+        await page.click('#closeSmallModal')
+		await page.waitForFunction(() => !document.querySelector('#example-modal-sizes-title-sm'))
         
 
 
