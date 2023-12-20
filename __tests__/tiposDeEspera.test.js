4213aff322dc0cbc655bdf4ad35287d2a54b58ee
John Alexander Pardo Gonzalez <j.pardog@uniandes.edu.co>
2023-12-20 05:29:52 -0500

    Esperar por los elemntos

---
+++ __tests__/tiposDeEspera.test.js
@@ -0,0 +1,35 @@
+const puppeteer = require("puppeteer")
+
+describe("Tipos de espera", () => {
+	it("Mostrar todos los diferentes tios de espera", async () => {
+		const browser = await puppeteer.launch({
+			headless: false, //que se abra o no el navedagor en mi pantalla (ginete sin cabeza). headless: false abre el navegador
+			defaultViewport: null, //modifica el ancho interno de la ventana para ajustarse al que se abre
+		})
+		const page = await browser.newPage()
+        await page.goto("https://www.yahoo.com/", {waitUntil: 'networkidle0'})
+
+		//Espera explicita
+		await page.waitForTimeout(500)
+        
+		//Espera por un  css selector
+		await page.waitForSelector("#ybar-logo > img._yb_2qzib._yb_iun8j")
+        
+		//Espera por un xpath
+		await page.waitForXPath('//*[@id="ybar-logo"]/img[1]')
+        
+		//Otra pagina
+		await page.goto("https://demoqa.com/modal-dialogs", { waitUntil: "networkidle0" })
+		//Valida ademas que el Dom aparezca el selector tambien que sea 100% visible, tambien se puede 
+        //cambiar la propiedad de "visible" por "hidden" para validar que este en el DOM pero que no sea visible
+		const button = await page.waitForSelector("#showSmallModal", { visible: true })
+		//await page.waitForSelector('//*[@id="showSmallModal"]', { visible: true })
+        await button.click()
+		await page.waitForTimeout(500)
+        
+        
+
+
+		await browser.close()
+	}, 40000)
+})
